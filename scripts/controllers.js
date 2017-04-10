423903656b353f8de50891a8af24e9cdc38f42ba
raducheptanariu <radu.cheptanariu@gmail.com>
2017-04-11 01:37:12 +0300

    sds

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -65,7 +65,7 @@ angular.module('app.controllers', [])
     .controller('Error404Ctrl', ['$scope', '$location', '$window', function ($scope, $location, $window) {
         $scope.$root.title = 'Error 404: Page Not Found';
         $scope.$on('$viewContentLoaded', function () {
-            $window.ga('send', 'pageview', { 'page': $location.path(), 'title': $scope.$root.title });
+            //$window.ga('send', 'pageview', { 'page': $location.path(), 'title': $scope.$root.title });
         });
     }])
 
