7dae2dfa7cdc25de6e717d8f17ee93890dc76551
raducheptanariu <radu.cheptanariu@gmail.com>
2017-04-15 20:24:05 +0300

    navbar sticky

--- scripts/directives.js
+++ scripts/directives.js
@@ -6,4 +6,61 @@ angular.module('app.directives', [])
         return function (scope, elm, attrs) {
             elm.text(version);
         };
-    }]);
+    }])
+
+    .directive('stickyOnScroll', [function () {
+        return {
+            restrict: 'A',
+            scope:{
+                stickyOnScroll: '@',
+                stickyClass: '@',
+                stickyHeight: '=',
+                stickyAdaptMargin: '@'
+            },
+            link: function (scope, elm, attrs) {
+                var pixels = parseInt(scope.stickyOnScroll);
+                var adapt = $('#' + scope.stickyAdaptMargin);
+                adapt.css('marginTop', scope.stickyMargin);
+                var height = elm.height() + 20;
+
+                $(window).scroll(function () {
+                    if ($(window).scrollTop() > pixels) {
+                        adapt.css('marginTop', height);
+                        elm.addClass(scope.stickyClass);
+                        //scope.$apply(function () {
+                        //    scope.stickyHeight = elm.height();
+                        //});
+                    }
+                    if ($(window).scrollTop() < pixels + 1) {
+                        adapt.css('marginTop', 0);
+                        elm.removeClass(scope.stickyClass);
+                        //scope.$apply(function () {
+                        //    scope.stickyHeight = 0;
+                        //});
+                    }
+                });
+            }
+        };
+    }])
+
+    .directive('stickyMargin', [function () {
+        return {
+            restrict: 'A',
+            scope: {
+                stickyMargin: '='
+            },
+            link: function (scope, elm, attrs) {
+                var initMargin = elm[0].style.marginTop;
+
+                scope.$watch(
+                    function () {
+                        return scope.stickyMargin;
+                    },
+                    function () {
+                        elm.css('marginTop', scope.stickyMargin);
+                    });
+            }
+        }
+    }])
+
+;
