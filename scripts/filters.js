aa0ab1fc2f48b6eb90be7d793bb887c73fe25963
raducheptanariu <radu.cheptanariu@gmail.com>
2017-04-19 00:46:10 +0300

    asd

--- scripts/filters.js
+++ scripts/filters.js
@@ -20,12 +20,50 @@ angular.module('app')
         }
     }])
 
-    .filter('intToDate', [function () {
+    .filter('intToInstaDate', [function () {
         return function (text) {
-            var date = new Date(1000 * parseInt(text));
+            var date = intToDate(text);
+            var today = new Date();
 
-            return date.toLocaleString();
+            var timeDiff = Math.abs(today.getTime() - date.getTime());
+            var daysDiff = Math.floor(timeDiff / (1000 * 3600 * 24));
+
+            if (daysDiff == 0) {
+                return "Posted today";
+            }
+            else if (daysDiff == 1) {
+                return "Posted 1 day ago";
+            }
+            else if (daysDiff <= 7) {
+                return "Posted " + daysDiff + " days ago";
+            }
+            else {
+                return "Posted on " + dateToStringMonths(date)
+            }
         }
     }])
 
+
+
 ;
+
+function intToDate(int) {
+    var date = new Date(1000 * parseInt(int));
+
+    return date;
+}
+
+function dateToStringMonths(date) {
+    var monthNames = [
+        "January", "February", "March",
+        "April", "May", "June", "July",
+        "August", "September", "October",
+        "November", "December"
+    ];
+
+    var day = date.getDate();
+    var monthIndex = date.getMonth();
+    var year = date.getFullYear();
+
+    return day + ' ' + monthNames[monthIndex] + ' ' + year;
+}
