1e66cdfa117dab91368d2af03be5244895e758df
raducheptanariu <radu.cheptanariu@gmail.com>
2017-04-21 01:15:57 +0300

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+// Include gulp
+var gulp = require('gulp');
+ // Include plugins
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+ // Concatenate & Minify JS
+gulp.task('scripts', function() {
+    return gulp.src([
+		"scripts/translations.js",
+		"scripts/vendor/jquery.min.js",
+		"scripts/vendor/angular.min.js",
+		"scripts/vendor/angular-route.min.js",
+		"scripts/vendor/angular-translate.min.js",
+		"scripts/vendor/angular-animate.min.js",
+		"scripts/vendor/angular-touch.min.js",
+		"scripts/vendor/ui-bootstrap-tpls-2.5.0.min.js",
+		"scripts/vendor/bootstrap.min.js",
+		"scripts/vendor/instafeed.min.js" ,
+		"scripts/vendor/ng-instafeed.min.js" ,
+		"scripts/vendor/angular-youtube-embed.min.js" ,
+		"scripts/app.js",
+		"scripts/services.js",
+		"scripts/filters.js",
+		"scripts/controllers.js",
+		"scripts/directives.js"
+	])
+		.pipe(concat('bundle.js'))
+			.pipe(rename({suffix: '.min'}))
+			.pipe(uglify())
+			.pipe(gulp.dest('build'));
+});
+ // Default Task
+gulp.task('default', ['scripts']);
