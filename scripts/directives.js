44b0d87d43f74cea434b1c590e410f11651d5ac4
raducheptanariu <radu.cheptanariu@gmail.com>
2017-04-26 23:31:18 +0300

    sad

--- scripts/directives.js
+++ scripts/directives.js
@@ -369,4 +369,26 @@ angular.module('app')
             }
         }
     }])
+
+    .directive('discussLoader', [function () {
+        return {
+            restrict: 'E',
+            scope: {
+
+            },
+            link: function (scope, elm, attrs) {
+                var disqus_config = function () {
+                    this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+                    this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+                };
+
+                var d = document, s = d.createElement('script');
+
+                s.src = 'https://lauraserban-com.disqus.com/embed.js';
+
+                s.setAttribute('data-timestamp', +new Date());
+                (d.head || d.body).appendChild(s);
+            }
+        }
+    }])
 ;
